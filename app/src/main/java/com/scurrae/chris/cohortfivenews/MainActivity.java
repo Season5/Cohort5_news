0263693afdacfd6de9060602d22fef6efdebf5a3
CKiilu <cndeti@gmail.com>
2016-03-16 18:02:21 +0300

    Replace faulty appintro

---
+++ app/src/main/java/com/scurrae/chris/cohortfivenews/MainActivity.java
@@ -0,0 +1,15 @@
+package com.scurrae.chris.cohortfivenews;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by chris on 3/16/16.
+ */
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
